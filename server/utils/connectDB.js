9ec48388b81f4a3b3e1f4d72082b4b8ce2cc27d8
Samaysrivastava <samay8193@gmail.com>
2026-04-30 19:55:06 +0530

    Initial Commit

---
+++ server/utils/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+// console.log(process.env.MONGODB_URI);
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(process.env.MONGODB_URI);
+
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("DB Error: " + error);
+  }
+};
+
+export default dbConnection;
