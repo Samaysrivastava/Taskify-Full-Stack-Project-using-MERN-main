fbc21223a1e40c0733d98c0bd253e046c9dc9268
Samaysrivastava <samay8193@gmail.com>
2026-04-30 21:10:34 +0530

    some minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -16,13 +16,7 @@ const port = process.env.PORT || 2000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+app.use(cors());
 app.get('/', (req, res) => {
     res.send("hello from server");
 });
