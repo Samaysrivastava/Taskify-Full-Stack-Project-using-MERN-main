ec8cd7bbc1574a2acde862270eaf45834808c22b
Samaysrivastava <samay8193@gmail.com>
2026-04-30 21:31:25 +0530

    some minor fixes

--- server/index.js
+++ server/index.js
@@ -18,9 +18,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.get('/', (req, res) => {
